53e959a621b1d0ee36eadc5c60aede4400854b18
meldyssey <meldyssey15@gmail.com>
2025-01-04 14:38:54 +0900

    console.log 오류 수정

--- src/components/main/MainSlide.js
+++ src/components/main/MainSlide.js
@@ -1,12 +1,12 @@
 // import Swiper core and required modules
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/scss';
-import 'swiper/scss/pagination';
-import 'swiper/scss/navigation';
-import '../../scss/main/mainslide.scss';
-import { Link } from 'react-router-dom';
+import "swiper/scss";
+import "swiper/scss/pagination";
+import "swiper/scss/navigation";
+import "../../scss/main/mainslide.scss";
+import { Link } from "react-router-dom";
 
 function MainSlide(props) {
     return (
@@ -23,24 +23,29 @@ function MainSlide(props) {
                     disableOnInteraction: false, // 사용자 상호작용 후에도 자동재생 계속
                 }}
                 loop={true} // 슬라이드를 무한 반복
-                onSwiper={(swiper) => // console.log(swiper)}
+                onSwiper={(swiper) => console.log(swiper)}
             >
                 <SwiperSlide>
-                    <Link to={'/bath-body'}>
-                        <div className="slide-ftitle">행복한 홀리데이를 위해</div>
+                    <Link to={"/bath-body"}>
+                        <div className="slide-ftitle">
+                            행복한 홀리데이를 위해
+                        </div>
                         <div className="slide-ftext">배스 앤 바디 보기</div>
                         <img src={`/imgs/main/main_slide2.jpg`} alt="Slide 2" />
                     </Link>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <Link to={'/product/45'}>
+                    <Link to={"/product/45"}>
                         <div className="slide-title">히노키 앤 시더우드</div>
                         <div className="slide-text">더 알아보기</div>
-                        <img src={`/imgs/main/main_slide1.avif`} alt="Slide 1" />
+                        <img
+                            src={`/imgs/main/main_slide1.avif`}
+                            alt="Slide 1"
+                        />
                     </Link>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <Link to={'/best-seller'}>
+                    <Link to={"/best-seller"}>
                         <div className="slide-title">베스트셀러</div>
                         <div className="slide-text">자세히 보기</div>
                         <img src={`/imgs/main/main_slide3.jpg`} alt="Slide 3" />
